acd6a19745ac49ddfe814f4f4bab6586de7ed1ec
Satansoft <scorpovi4@gmail.com>
2018-01-07 17:07:45 +0200

    without real images

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,8 @@ $(window).on('load', function () {
     console.log('gg');
     $('.js-popup').magnificPopup({
         type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-fade',
         gallery: {
             enabled: true
         }
