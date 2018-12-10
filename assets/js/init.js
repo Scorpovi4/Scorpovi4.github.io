e6c298752f60479f796db3d6bc85c0a93fab1ea4
satansoft <nazarov.s.wezom@gmail.com>
2018-12-10 11:11:36 +0200

    static

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$('.js-tab').on('click', function (e) {
 		e.preventDefault();
 		const href = $(this).attr('href'),
-			  whole = $('[data-tabContent]');
+			  	whole = $('[data-tabContent]');
 
 			  $('.js-tab').each(function (i, el) {
 				$(el).removeClass('active-tab');
@@ -22,7 +22,42 @@ $(document).ready(function() {
 						$(el).slideDown(700);
 					}
 				});
-
 	});
 
+	setTimeout(function () {
+		$('body').addClass('show-page');
+	}, 400);
+
+	if($('.double-slider').length > 0) {
+		$('.double-slider').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				fade: true,
+				asNavFor: '.double-slider-nav'
+		  });
+		  $('.double-slider-nav').slick({
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				asNavFor: '.double-slider',
+				dots: false,
+				focusOnSelect: true,
+				responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 3
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 2
+					}
+				}
+			]
+		});
+	}
+
+	new WOW().init();
 })
